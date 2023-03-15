955bb46a5f590077dddd8b4f05278ab40f532bf7
C.R.S.M <cassiel1998@hotmail.com>
2023-03-15 10:15:20 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('payflow')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
